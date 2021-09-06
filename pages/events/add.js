66162266557bc7469d3a8a372736ece5d1025664
Nasimul Haque <alnasimul@gmail.com>
2021-09-06 12:16:09 +0600

    initial commit

---
+++ pages/events/add.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+
+const AddEventPage = () => {
+    return (
+        <Layout title='Add Event'>
+            <h1>
+                Add Event
+            </h1>
+        </Layout>
+    );
+}
+
+export default AddEventPage;
