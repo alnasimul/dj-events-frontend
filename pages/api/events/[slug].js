625ee80ebc397cb3d3ac29fcae43d1db988eddc0
Nasimul Haque <alnasimul@gmail.com>
2021-09-06 23:19:13 +0600

    exploring api routes

---
+++ pages/api/events/[slug].js
@@ -0,0 +1,15 @@
+const { events } = require("./data.json");
+
+export default function handler(req, res) {
+  const event = events.filter( se => se.slug === req.query.slug );
+
+  if (req.method === "GET") {
+    res.status(200).json(event);
+  }
+  else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({
+      message: `Method ${req.method} is not allowed`,
+    });
+  }
+}
