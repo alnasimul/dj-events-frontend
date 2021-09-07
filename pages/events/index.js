36b5ca7eb31a953358c19fcc78e58391878f15d0
Nasimul Haque <alnasimul@gmail.com>
2021-09-07 14:01:30 +0600

    added new feature

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,28 @@
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const EventsPage = () => {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    );
+export default function EventsPage({events}) {
+  return (
+   <Layout>
+     <h1>Events</h1>
+     {events.length === 0 && <h3>No events to show</h3> }
+
+     {
+       events.map(event => (
+         <EventItem key={event.id} event={event}/>
+       ))
+     }
+   </Layout>
+  )
 }
 
-export default EventsPage;
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props:{events},
+    revalidate: 1
+  }
+}
